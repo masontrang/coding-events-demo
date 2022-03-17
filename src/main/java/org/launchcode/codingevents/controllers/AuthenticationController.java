c7d94c23c11ab9839e44e6d23e0e5678ec8a568b
masontrang <mt9260@gmail.com>
2022-03-16 21:25:08 -0400

    Authentication Exercises

---
+++ src/main/java/org/launchcode/codingevents/controllers/AuthenticationController.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingevents.controllers;
+
+import org.launchcode.codingevents.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+}
